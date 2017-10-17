2226dfc3ef0b0330b24f32f0150cc4b95b7f1748
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-10-17 11:05:37 +0200

    feat: add patreon tag

---
+++ scripts/patreon.js
@@ -0,0 +1,5 @@
+hexo.extend.tag.register('patreon', args => 
+  `<h4 id="Like-what-you-see-Support-us-on-Patreon"><a href="#Like-what-you-see-Support-us-on-Patreon" class="headerlink" title="Like what you see? Support us on Patreon!"></a>Like what you see? Support us on Patreon!</h4>
+   <p>MachineLabs, Inc is a small, dedicated and 100 % bootstrapped company. Our main goal is to provide the community with better tools to move the whole Machine Learning ecosystem forward.</p>
+   <p>You can make a difference and support us on our mission, <a href="https://patreon.com/machinelabs" target="_blank" rel="external">become a Patreon</a>!</p>`
+);
